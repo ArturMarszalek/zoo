75bfd010e939f9a3f8bda129a45f5f2841bac922
Norbert <syskaxiii@gmail.com>
2020-02-22 10:46:47 +0100

    added count animals + test

--- src/main/java/ZOO/Zoo.java
+++ src/main/java/ZOO/Zoo.java
@@ -6,6 +6,7 @@ import ZOO.Species.PolarBear;
 import ZOO.Species.TeddyBear;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Zoo {
     ArrayList<Bear> ListOfAllAnimals = new ArrayList<>();
@@ -19,17 +20,39 @@ public class Zoo {
         ListOfAllAnimals.add(new BronzeBear());
         ListOfAllAnimals.add(new BronzeBear());
         ListOfAllAnimals.add(new BronzeBear());
-        for (int i = 0; i<20;i++){
+        for (int i = 0; i < 20; i++) {
             ListOfAllAnimals.add(new TeddyBear());
         }
 
     }
 
-    public int getNumberOfAllAnimals(){
-        int count = 0;
-        for (int i=0;i<ListOfAllAnimals.size();i++){
-            count++;
+    public int getNumberOfAllAnimals() {
+
+        return ListOfAllAnimals.size();
+    }
+
+
+    public HashMap<String, Integer> getAnimalCount() {
+        HashMap<String, Integer> animalCount = new HashMap<>();
+        //Normalny for
+        /*for (int i = 0; i < ListOfAllAnimals.size(); i++) {
+            if (animalCount.get(ListOfAllAnimals.get(i).getName()) != null) {
+                animalCount.put(ListOfAllAnimals.get(i).getName(), animalCount.get(ListOfAllAnimals.get(i).getName()) + 1);
+            }else{
+                animalCount.put(ListOfAllAnimals.get(i).getName(),1);
+            }
         }
-        return count;
+        */
+
+        //to samo foreach'em
+        for (Bear bear : ListOfAllAnimals){
+            if (animalCount.get(bear.getName()) != null){
+                animalCount.put(bear.getName(),animalCount.get(bear.getName())+1);
+            }else{
+                animalCount.put(bear.getName(),1);
+            }
+        }
+
+        return animalCount;
     }
 }
--- src/test/java/ZOO/BearTest.java
+++ src/test/java/ZOO/BearTest.java
@@ -7,6 +7,7 @@ import ZOO.Species.TeddyBear;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -231,8 +232,21 @@ class BearTest {
         //given
         Zoo zoo = new Zoo();
         //when
-        zoo.getNumberOfAllAnimals();
+        int result = zoo.getNumberOfAllAnimals();
         //then
-        assertEquals(28,zoo.getNumberOfAllAnimals());
+        assertEquals(28, result);
+    }
+
+    @Test
+    void ShouldReturnNubmerOfTeddyBears() {
+        //given
+        Zoo zoo = new Zoo();
+        //when
+        HashMap<String, Integer> result = zoo.getAnimalCount();
+        //then
+        assertEquals(2, result.get("Black Bear"));
+        assertEquals(2, result.get("Polar Bear"));
+        assertEquals(4, result.get("Brown Bear"));
+        assertEquals(20, result.get("Teddy Bear"));
     }
 }
