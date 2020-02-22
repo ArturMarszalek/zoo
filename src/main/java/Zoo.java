3d5fffdcc5cdef6e3e7607dd9316e9ad58b479e7
Szorstki <baiokai@gmail.com>
2020-02-22 12:49:50 +0100

    From file

--- src/main/java/Zoo.java
+++ src/main/java/Zoo.java
@@ -1,40 +1,62 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Zoo {
 
-    ArrayList<Bear> animals = new ArrayList<Bear>();
+    ArrayList<Bear> animals = new ArrayList<>();
+
+    public Zoo(){
+        initializeAnimalsInZooFromStaticFile();
+    }
 
 
-    public Zoo() {
-        animals.add(new Blackbear());
-        animals.add(new Blackbear());
-        animals.add(new PolarBear());
-        animals.add(new PolarBear());
-        animals.add(new BrownBear());
-        animals.add(new BrownBear());
-        animals.add(new BrownBear());
-        animals.add(new BrownBear());
-        for (int i = 0; i < 20; i++) {
-            animals.add(new TeddyBear());
+    private void initializeAnimalsInZooFromStaticFile() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("D:\\!SDA\\zoo1\\src\\main\\resources\\Animals"));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] animalQuantity = line.split(" ");
+                String animalType = animalQuantity[0];
+                int animalCount = Integer.parseInt(animalQuantity[1]);
+                for (int i = 0; i < animalCount; i++) {
+                    Bear animalToAdd = createAnimalByType(animalType);
+                    animals.add(animalToAdd);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    private Bear createAnimalByType(String animalType) throws Exception {
+        switch (animalType) {
+            case "BlackBear":
+                return new Blackbear();
+            case "PolarBear":
+                return new PolarBear();
+            case "BrownBear":
+                return new BrownBear();
+            case "TeddyBear":
+                return new TeddyBear();
+            default: throw new Exception("can't create animal by type");
         }
     }
 
-    public int getBymberOfAllAnimals() {
+    public int getNumberOfAllAnimals() {
         return animals.size();
     }
 
-    public Map<String, Integer> getAnimalsCount(){
-        Map<String,Integer> animalMap = new HashMap<>();
-        for (Bear bear : animals){
-            if (animalMap.get(bear.getName()) != null){
-                animalMap.put(bear.getName(),animalMap.get(bear.getName())+1);
-            }else{
-                animalMap.put(bear.getName(),1);
+    public Map<String, Integer> getAnimalsCount() {
+        Map<String, Integer> animalMap = new HashMap<>();
+        for (Bear bear : animals) {
+            if (animalMap.get(bear.getName()) != null) {
+                animalMap.put(bear.getName(), animalMap.get(bear.getName()) + 1);
+            } else {
+                animalMap.put(bear.getName(), 1);
             }
         }
-
         return animalMap;
     }
 }
