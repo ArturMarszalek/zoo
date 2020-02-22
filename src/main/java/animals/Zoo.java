0cb6356966288b6c1a75872d6b4ae0f3c310d61c
Paweł <frankowski.pawel@gmail.com>
2020-02-22 12:11:46 +0100

    add animals from file

--- src/main/java/animals/Zoo.java
+++ src/main/java/animals/Zoo.java
@@ -3,40 +3,69 @@ package animals;
 import animals.bears.*;
 import animals.bears.Animal;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 public class Zoo {
 
     ArrayList<Animal> animals = new ArrayList<>();
 
     public Zoo() {
-        for (int i=0; i<2; i++){
-        animals.add(new PolarBear());}
-        for (int i=0; i<4; i++){
-            animals.add(new BrownBear());}
-        for (int i=0; i<20; i++){
-        animals.add(new TeddyBear());}
-        for (int i=0; i<2; i++){
-        animals.add(new BlackBear());}
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("src/main/resources/Animals.txt"));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] animalQuantity = line.split("/");
+                String animalType = animalQuantity[0];
+                int animalCount = Integer.parseInt(animalQuantity[1]);
+
+                if (animalType.equals("pluszak")){
+                    for (int i=0; i<animalCount; i++) {
+                    animals.add(new TeddyBear()); }}
+                if (animalType.equals("czarny niedźwiedż")){
+                    for (int i=0; i<animalCount; i++) {
+                        animals.add(new BlackBear()); }}
+                if (animalType.equals("polarny niedźwiedż")){
+                    for (int i=0; i<animalCount; i++) {
+                        animals.add(new PolarBear()); }}
+                if (animalType.equals("brązowy niedźwiedż")){
+                    for (int i=0; i<animalCount; i++) {
+                        animals.add(new BrownBear()); }}
+
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+
+//        for (int i = 0; i < 2; i++) {
+//            animals.add(new PolarBear());
+//        }
+//        for (int i = 0; i < 4; i++) {
+//            animals.add(new BrownBear());
+//        }
+//        for (int i = 0; i < 20; i++) {
+//            animals.add(new TeddyBear());
+//        }
+//        for (int i = 0; i < 2; i++) {
+//            animals.add(new BlackBear());
+//        }
     }
 
     public int getNumberOfAllAnimals() {
-
         return animals.size();
     }
 
     public HashMap<String, Integer> getAnimalsCount() {
-        HashMap<String, Integer> allAnimals = new HashMap<String, Integer>() {
+        HashMap<String, Integer> allAnimalsMap = new HashMap<String, Integer>() {
         };
         for (Animal animal : animals) {
-            if (allAnimals.containsKey(animal.getName())) {
-                allAnimals.put(animal.getName(), (allAnimals.get(animal.getName()) + 1));
-            } else {
-                    allAnimals.put(animal.getName(), 1);
+            allAnimalsMap.putIfAbsent(animal.getName(), 0);
+                allAnimalsMap.put(animal.getName(), (allAnimalsMap.get(animal.getName()) + 1));
             }
-        }
-        return allAnimals;
+        return allAnimalsMap;
     }
 }
---
+++ src/main/java/animals/bears/Main.java
@@ -0,0 +1,9 @@
+package animals.bears;
+
+import animals.Zoo;
+
+public class Main {
+    public static void main(String[] args) {
+        Zoo zoo = new Zoo();
+    }
+}
